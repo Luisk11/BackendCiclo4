03fb87b6fe72dd34dd24b7a2213d64f1292bb38c
Luisk11 <mvluiscarlos@gmail.com>
2021-11-17 09:34:32 -0500

    Actualizacion

--- src/routes/outcome.routes.js
+++ src/routes/outcome.routes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const outcomeController = require('../controllers/outcome.controller.js')
 const router = express.Router()
 
-router.post('/', outController.add)
-router.get('/', outController.list)
+router.post('/', outcomeController.add)
+router.get('/', outcomeController.list)
 router.get('/:id', outcomeController.find)
 
 module.exports = router
